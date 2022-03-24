a24841e1d10c09e5d5e3936a51aa98805fe093d6
Matt Sanchez <mattasanchez77@gmail.com>
2022-03-24 15:10:48 -0700

    Up to 1.9
    Before refactor

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
-const Header = ({text}) => {
-	return <h1>{text}</h1>
-}
+const Header = ({text}) => (
+	<h1>{text}</h1>
+)
 
 const Button = (props) => (
 	<button onClick={props.handleClick}>
@@ -10,9 +10,27 @@ const Button = (props) => (
 	</button>
 )
 
-const Statistics = ({type, count}) => (
-	<p>{type} {count}</p>
-)
+const roundNum = (type, num) => {
+  let rounded = num.toFixed(2);
+  if (type === "positive") {
+    rounded += '%';
+  }
+  return rounded;
+}
+
+const Statistics = ({type, count}) => {
+  if (isNaN(count) || count === 0) {
+    return <p>No feedback given</p>
+  }
+
+  let final = count;
+
+  if (type === "average" || type === "positive") {
+    final = roundNum(type, count);
+  }
+
+  return <p>{type} {final}</p>;
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -20,13 +38,33 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-	const increaseByOne = (newVal) => setGood(newVal)
+	const increaseByOne = (type) => {
+    if (type === "good") {
+      setGood(good + 1)
+    } else if (type === "neutral") {
+      setNeutral(neutral + 1)
+    } else {
+      setBad(bad + 1)
+    }
+  }
+
+  const getTotal = () => good + neutral + bad
+  const getAverage = () => (( (good*1) + (bad*-1) ) / getTotal())
+  const getPositive = () => (good / getTotal()) * 100
 
   return (
     <div>
       <Header text="give feedback"></Header>
-			<Button handleClick={() => increaseByOne(1000)} text={"good"}></Button>
-			<Statistics type="good" count={good}></Statistics>
+			<Button handleClick={() => increaseByOne("good")} text={"good"}></Button>
+      <Button handleClick={() => increaseByOne("neutral")} text={"neutral"}></Button>
+      <Button handleClick={() => increaseByOne("bad")} text={"bad"}></Button>
+			<Header text="statistics"></Header>
+      <Statistics type="good" count={good}></Statistics>
+      <Statistics type="neutral" count={neutral}></Statistics>
+      <Statistics type="bad" count={bad}></Statistics>
+      <Statistics type="all" count={getTotal()}></Statistics>
+      <Statistics type="average" count={getAverage()}></Statistics>
+      <Statistics type="positive" count={getPositive()}></Statistics>
     </div>
   )
 }
