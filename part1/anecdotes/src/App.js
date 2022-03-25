080e5d5b02de22678e2451ae36f1789ff6f4fde0
Matt Sanchez <mattasanchez77@gmail.com>
2022-03-25 12:03:44 -0700

    Part 1 Complete

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const MostPopular = ({anecdotes, votes}) => {
+  let current_votes = Object.values(votes);
+  let max_votes = Math.max.apply(null, current_votes);
+  let max_anecdote = current_votes.indexOf(max_votes);
+
+  return <p>{anecdotes[max_anecdote]}</p>
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -11,12 +25,42 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
    
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0
+  });
+
+  const getRandNumber = (end) => Math.floor(Math.random() * end);
+
+  const setRandAnecdote = (range) => {
+    const rand = getRandNumber(range);
+    setSelected(rand);
+  }
+
+  const vote = () => {
+    const copy = { ...votes };
+    copy[selected] += 1;
+    setVotes(copy);
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button handleClick={() => vote()} text={"vote"}/>
+      <Button handleClick={() => setRandAnecdote(anecdotes.length)} text={"next anecdote"}/>
+
+      <h1>Anecdote with most votes</h1>
+      <MostPopular anecdotes={anecdotes} votes={votes}/>
     </div>
+    
   )
 }
 
